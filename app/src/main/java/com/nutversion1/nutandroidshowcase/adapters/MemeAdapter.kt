62d3472e4ebdd8fb927dddf396e85ca90d93410b
Nuttapong Phisitbutr <nuttp305@iteam.postbox.in.th>
2022-11-01 16:24:02 +0700

    Created ProgrammingMemes' recycler view

---
+++ app/src/main/java/com/nutversion1/nutandroidshowcase/adapters/MemeAdapter.kt
@@ -0,0 +1,58 @@
+package com.nutversion1.nutandroidshowcase.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.nutversion1.nutandroidshowcase.R
+import com.nutversion1.nutandroidshowcase.api.responses.GetMemesResponse
+
+class MemeAdapter() : RecyclerView.Adapter<MemeAdapter.MemeViewHolder>(){
+    private val memes = mutableListOf<GetMemesResponse>()
+
+    fun setData(memes: List<GetMemesResponse>){
+        this.memes.clear()
+        this.memes.addAll(memes)
+
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MemeViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.view_meme_item, parent, false)
+        return MemeViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: MemeViewHolder, position: Int) {
+
+
+        holder.bindData(memes[position])
+    }
+
+    override fun getItemCount(): Int {
+        return memes.size
+    }
+
+    class MemeViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+        private val titleText: TextView by lazy {
+            itemView.findViewById(R.id.title_text)
+        }
+
+        private val memeImage: ImageView by lazy {
+            itemView.findViewById(R.id.meme_image)
+        }
+
+        fun bindData(meme: GetMemesResponse){
+            titleText.text = meme.title
+
+            Glide.with(itemView.context)
+                .load(meme.link)
+                .placeholder(R.mipmap.ic_launcher)
+                .fitCenter()
+                .into(memeImage)
+        }
+    }
+}
+
--- app/src/main/java/com/nutversion1/nutandroidshowcase/fragments/ProgrammingMemesFragment.kt
+++ app/src/main/java/com/nutversion1/nutandroidshowcase/fragments/ProgrammingMemesFragment.kt
@@ -1,19 +1,14 @@
 package com.nutversion1.nutandroidshowcase.fragments
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.TextView
 import androidx.fragment.app.viewModels
 import com.nutversion1.nutandroidshowcase.MyViewModel
-import com.nutversion1.nutandroidshowcase.R
-import com.nutversion1.nutandroidshowcase.databinding.FragmentAboutBinding
+import com.nutversion1.nutandroidshowcase.adapters.MemeAdapter
 import com.nutversion1.nutandroidshowcase.databinding.FragmentProgrammingMemesBinding
-import com.nutversion1.nutandroidshowcase.databinding.FragmentRandomQuoteBinding
 
 
 class ProgrammingMemesFragment : Fragment() {
@@ -31,6 +26,9 @@ class ProgrammingMemesFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val memeAdapter = MemeAdapter()
+        binding.memeList.adapter = memeAdapter
+
         binding.generateButton.setOnClickListener {
             val viewModel: MyViewModel by viewModels { MyViewModel.Factory()}
             viewModel.getMemes()
@@ -38,7 +36,7 @@ class ProgrammingMemesFragment : Fragment() {
 
         val viewModel: MyViewModel by viewModels {MyViewModel.Factory()}
         viewModel.getMemesResponse.observe(viewLifecycleOwner) {
-            Log.d("myDebug", "total: ${it.size}")
+            memeAdapter.setData(it)
         }
     }
 
