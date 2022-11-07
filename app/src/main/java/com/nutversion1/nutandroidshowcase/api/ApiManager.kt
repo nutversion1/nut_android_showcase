3d721fe4a46c0f5b40577ef865db0fbbf1037c30
Nuttapong Phisitbutr <nuttp305@iteam.postbox.in.th>
2022-11-07 13:30:49 +0700

    Added api request timeout

--- app/src/main/java/com/nutversion1/nutandroidshowcase/api/ApiManager.kt
+++ app/src/main/java/com/nutversion1/nutandroidshowcase/api/ApiManager.kt
@@ -6,6 +6,7 @@ import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.converter.moshi.MoshiConverterFactory
+import java.util.concurrent.TimeUnit
 
 object ApiManager {
     fun getRandomQuoteService(): RandomQuoteService {
@@ -101,6 +102,9 @@ object ApiManager {
 
     fun getFootballService(): FootballService {
         val okHttpClient = OkHttpClient.Builder().apply {
+            connectTimeout(3, TimeUnit.SECONDS)
+            readTimeout(10, TimeUnit.SECONDS)
+            writeTimeout(5, TimeUnit.SECONDS)
             addInterceptor(getHttpLoggingInterceptor())
             addInterceptor(getRapidApiKeyInterceptor())
         }.build()
